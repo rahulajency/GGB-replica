9a03b6204ed13e67321b1fb4381f278e33cd86f9
rahulbandodkar <59685264+rahull04@users.noreply.github.com>
2021-02-27 13:11:31 +0530

    category

--- src/components/dishcategory-box/dishcategory-box.js
+++ src/components/dishcategory-box/dishcategory-box.js
@@ -2,9 +2,11 @@
 import * as React from 'react';
 import './dishcategory-box.scss';
 
-export class DishCategoryBox extends React.Component{
+export default class DishCategoryBox extends React.Component{
+
     render() {
-        let {heading,content,imgs,cardTitle,cardPrice,cardOGPrice} = this.props;
+        let {heading,content,available,imgs,cardTitle,cardPrice,cardOGPrice} = this.props;
+        let days = ['mon','tue','wed','thu','fri','sat','sun'];
         return (
         <div className='dish-category'>
             <div className='dish-category-heading'>
@@ -14,7 +16,16 @@ export class DishCategoryBox extends React.Component{
                 {content}
             </div>
             <div className='dish-category-availability'>
-
+                <div className="tabs">
+                    <div className=""><span className="font-size-15 pr-2">Available on:</span></div>
+                    <div className="">
+                        {
+                            days.map( (day) => {
+                                return <label key={day} className={"tab" + ( available.includes(day) ? ' bold' : ' strike' )} >{day}</label>
+                            } )
+                        }
+                    </div>
+                </div>
             </div>
             <div className='dish-category-carousel'>
 
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -4,10 +4,31 @@ import Header from '../../components/header/header';
 import Description from '../../components/description/description';
 import Quote from '../../components/quote/quote';
 import DaySelectionBtn from '../../components/days-selection-button/days-selection-button';
+import DishCategory from '../../components/dishcategory-box/dishcategory-box';
 
 export default class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishData:''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://demo1586672.mockable.io/dishes')
+      .then( (resp) => resp.json() )
+      .then( (resp) => {
+        console.log(resp.msg,resp.data);
+        this.setState({
+          dishData:resp.data
+        });
+      } )
+      .catch( err => console.log(err) );
+  }
+
   render(){
     let days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    let {dishData} = this.state;
     return (
       <div className="home-container">
         <div className='left'>
@@ -18,11 +39,20 @@ export default class Home extends React.Component {
             {
               days.map((day) => {
                 return(
-                  <DaySelectionBtn day={day} />
+                  <DaySelectionBtn day={day} key={day} />
                 )
               })
             }
-            
+          </div>
+          <div className='category-dishes-container'>
+          {
+            dishData && dishData.map( (dish) => {
+              return (
+                <DishCategory key={dish.id} heading={dish.heading} content={dish.content}
+                 available={dish.available_on} />
+              )
+            } )
+          }  
           </div>
         </div>
         <div className='right'>
