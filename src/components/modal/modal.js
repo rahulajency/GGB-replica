03a45f057771308a87d07ffff2024ab2124f3216
Rahul B <rahul@ajency.in>
2021-03-09 19:16:55 +0530

    side-panel

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -19,9 +19,10 @@ export default class modal extends React.Component{
     }
   }
 
-  inputHandler = (event,item) => {
+  inputHandler = (value,item) => {
     this.setState({
-      [item]:event.target.value
+      [item]:value,
+      [item+'Err']:''
     })
   }
 
@@ -29,21 +30,28 @@ export default class modal extends React.Component{
     let { toggleModal , sidePanelHandler } = this.props;
     let { size , day , slot } = this.state;
 
-    if( size != '' && day != '' && slot !='' ){
+    if(day == ''){
+      this.setState({
+        dayErr:'Please select a day'
+      })
+    }else if(slot == ''){
+      this.setState({
+        slotErr:'Please select a slot'
+      })
+    }else{
       await toggleModal();
       sidePanelHandler();
-    }else{
-      console.log('Invalid');
     }
   }
 
   render() {
     let { toggleModal , sidePanelHandler } = this.props;
     let { imageData , sizeErr , dayErr , slotErr } = this.state;
-    let { name , price , og_price , available_on } =  imageData;
+    let { name , price , size , og_price , available_on } =  imageData;
     let regPrice = ( parseInt(price) - ( parseInt(price) * 0.1 ) );
     let regOGPrice = ( parseInt(og_price) - ( parseInt(og_price) * 0.1 ) );
 
+    console.log('Size = '+size);
     return (
       <div className='modal-wrapper' >
         <div className='modal'>
@@ -59,23 +67,23 @@ export default class modal extends React.Component{
           </div>
           <div className='modal-size'>
             <div className='title' >{config.MODAL.SELECT_SIZE}</div>
-            <div>{sizeErr}</div>
+            <div className='err' >{sizeErr}</div>
             <div className='size-container'>
               <div className='size-item' >
-                <div className='size-item-input' ><input type='radio' className='size-radio' name='bowl' value='regular' onChange={(event)=>this.inputHandler(event,'size')} /><div>{config.MODAL.REGULAR_BOWL}</div></div>
+                <div className='size-item-input' ><input type='radio' className='size-radio' name='bowl' id='regular' value='regular' onChange={(event)=>this.inputHandler(event.target.value,'size')} checked={true} /><label htmlFor='regular' onClick={()=> this.inputHandler('regular','size') } className={ size === 'regular' ? 'green' : '' } >{config.MODAL.REGULAR_BOWL}</label></div>
                 <div className='size-price' >{config.SHARED_ITEMS.RUPEE}{regPrice}<span className='og' >{config.SHARED_ITEMS.RUPEE}{regOGPrice}</span></div>
               </div>
               <div className='size-item' >
-                <div className='size-item-input'><input type='radio' className='size-radio' name='bowl' value='super-bowl' onChange={(event)=>this.inputHandler(event,'size')} /><div>{config.MODAL.SUPER_BOWL}</div></div>
+                <div className='size-item-input'><input type='radio' className='size-radio' name='bowl' id='super-bowl' value='super-bowl' onChange={(event)=>this.inputHandler(event.target.value,'size')} /><label htmlFor='super-bowl' onClick={()=> this.inputHandler('super-bowl','size') }  className={ size === 'super-bowl' ? 'green' : '' }>{config.MODAL.SUPER_BOWL}</label></div>
                 <div className='size-price' >{config.SHARED_ITEMS.RUPEE}{price} <span className='og' >{config.SHARED_ITEMS.RUPEE}{og_price}</span></div>
               </div>
             </div>
           </div>
           <div className='modal-delivery'>
             <div className='delivery-item'>
-              <div className='txt' >{config.MODAL.SELECT_DELIVERY_DAY}</div>
-              <select className='select' onChange={(event)=>this.inputHandler(event,'day')} >
-                <option>{config.MODAL.CHOOSE_DAY}</option>
+              <div className='txt' >{config.MODAL.SELECT_DELIVERY_DAY} </div>
+              <select className='select' onChange={(event)=>this.inputHandler(event.target.value,'day')} >
+                <option hidden={true} >{config.MODAL.CHOOSE_DAY}</option>
                 {
                   available_on.map((day,index) => {
                     return(
@@ -84,14 +92,16 @@ export default class modal extends React.Component{
                   })
                 }
               </select>
+              <div className='err' >{dayErr}</div>
             </div>
             <div className='delivery-item'>
-              <div className='txt'>{config.MODAL.SELECT_DELIVERY_SLOT}</div>
-              <select className='select' onChange={(event)=>this.inputHandler(event,'slot')}>
-                <option>{config.MODAL.CHOOSE_SLOT}</option>
+              <div className='txt'>{config.MODAL.SELECT_DELIVERY_SLOT} </div>
+              <select className='select' onChange={(event)=>this.inputHandler(event.target.value,'slot')}>
+                <option hidden={true} >{config.MODAL.CHOOSE_SLOT}</option>
                 <option value={config.MODAL.LUNCH} >{config.MODAL.LUNCH}</option>
                 <option value={config.MODAL.DINNER} >{config.MODAL.DINNER}</option>
               </select>
+              <div className='err' >{slotErr}</div>
             </div>
           </div>
           <div className='modal-btn'>
